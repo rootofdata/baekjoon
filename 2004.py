128bfe5becfd2efabe3c3054ae9f864640d9c1de
dudtjr4915 <dudtjr4915@naver.com>
2022-05-15 01:25:42 +0900

    조합 0의 개수

--- 2004.py
+++ 2004.py
@@ -1,16 +1,16 @@
 #조합 0의 개수
 n,m=map(int,input().split())
-
 #2의 경우, #2! ->1 4! -> 3 6!-> 4 8!-> 7
 def split_two(x):
     two_cnt=0
-    while x>0:
+    while x!=0:
         x=x//2
         two_cnt+=x
     return two_cnt
+
 def split_five(x):
     five_cnt=0
-    while x>0:
+    while x!=0:
         x=x//5
         five_cnt+=x
     return five_cnt
